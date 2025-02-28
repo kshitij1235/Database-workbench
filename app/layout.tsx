040809d25024e343304937f56ac0006dd6d8eac0
Kshitij <65331304+kshitij1235@users.noreply.github.com>
2025-03-01 01:41:40 +0530

    fixed the hydatation error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,24 @@
-import type { Metadata } from "next"
-import "../styles/globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
-
-
+import type { Metadata } from "next";
+import "../styles/globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
 export const metadata: Metadata = {
-  title: 'Database_Workbench',
-  description: 'Let you design database.',
-  generator: '',
-}
+  title: "Database_Workbench",
+  description: "Let you design database.",
+};
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning={true}>
       <body>
-
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
